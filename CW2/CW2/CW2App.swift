6e416aa138b160341a02e7b230409ce26b1a6013
Fatima AlGarashi <63903553+fatmaalgrashi@users.noreply.github.com>
2022-09-08 04:02:29 +0300

    CW2 task 1 and 2

--- CW2/CW2/CW2App.swift
+++ CW2/CW2/CW2App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CW2App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView(dinar: Int())
+            ContentView(dinar: String())
          
         }
     }
--- CW2/CW2/ContentView.swift
+++ CW2/CW2/ContentView.swift
@@ -9,15 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var dinar : Int
+    @State var dinar = "0"
+    @State var usd = 0.0
+    
     
     var body: some View {
         
+        
         VStack{
-            TextField("العملة بالدينار",
-                      value: $dinar,
-                      formatter: NumberFormatter()
-            )
+            
+            Spacer()
+            Text("محول العملات العجيب!")
+                .font(.largeTitle)
+            
+            Spacer()
+            TextField("العملة بالدينار", text: $dinar)
+                .multilineTextAlignment(.center)
+                .font(.largeTitle)
+            
+            HStack{
+                
+                Image("usa")
+                    .resizable()
+                    .frame(width: 60, height: 30)
+                
+                Text("$")
+                    .font(.largeTitle)
+                Text(String(format: "%.2f", ((Double(dinar) ?? 0) * 3.28) ))
+                    .font(.largeTitle)
+            }
+            
+            HStack{
+                Image("uk")
+                    .resizable()
+                    .frame(width: 60, height: 30)
+                Text("£")
+                    .font(.largeTitle)
+                Text(String(format: "%.2f", ((Double(dinar) ?? 0) * 2.46) ))
+                    .font(.largeTitle)
+            }
+            
+            HStack{
+                
+                Image("eu")
+                    .resizable()
+                    .frame(width: 60, height: 50)
+                Text("€")
+                    .font(.largeTitle)
+                Text(String(format: "%.2f", ((Double(dinar) ?? 0) * 2.70) ))
+                    .font(.largeTitle)
+            }
+            Spacer()
+            Image("money")
+                .resizable()
+                .frame(width: 400, height: 270)
+            
         
         }
     }
@@ -25,6 +71,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(dinar: Int())
+        ContentView()
     }
 }
